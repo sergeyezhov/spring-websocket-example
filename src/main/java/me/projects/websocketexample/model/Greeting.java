8203d3382438fcfd40e7e8d3306e49d4609fb1c2
ssejov <CCT;jd!1>
2017-09-15 09:57:22 +0300

    sockjs app

---
+++ src/main/java/me/projects/websocketexample/model/Greeting.java
@@ -0,0 +1,12 @@
+package me.projects.websocketexample.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/me/projects/websocketexample/model/HelloMessage.java
@@ -0,0 +1,10 @@
+package me.projects.websocketexample.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
